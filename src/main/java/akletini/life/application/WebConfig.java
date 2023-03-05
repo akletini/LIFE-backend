5f9c4425062505d0a88bb03302a37c8ac66028f1
akletini <allen.k@freenet.de>
2023-03-05 17:31:43 +0100

    Sending mails in correct format

--- src/main/java/akletini/life/application/WebConfig.java
+++ src/main/java/akletini/life/application/WebConfig.java
@@ -1,12 +1,16 @@
 package akletini.life.application;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableAsync
+@EnableScheduling
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
